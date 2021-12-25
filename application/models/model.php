63c77bf13e3451d25700472e479cae3e4019445e
MarioV27 <84635915+MarioV27@users.noreply.github.com>
2021-12-25 17:30:05 +0700

    Update model.php

--- application/models/model.php
+++ application/models/model.php
@@ -1,7 +1,7 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class model  extends CI_Model{
+class lapor_model extends CI_Model{
     private $_table = "laporan";
 
     public $id;
